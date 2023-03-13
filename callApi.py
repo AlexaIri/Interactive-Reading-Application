dd436f32d205706bf56e5332764213cfe6b4479b
Alexandra Irimia <zcabair@ucl.ac.uk>
2023-03-13 20:11:47 +0000

    Add json file for configuring reading modes automatically

--- callApi.py
+++ callApi.py
@@ -1,16 +1,17 @@
 from readWithMe import ReadWithMe
 from metaverseGenerator import MetaverseGenerator
+import json
 import os
 
 MAIN_FOLDER_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__))))
 VOSK_PATH = os.path.join(MAIN_FOLDER_PATH, 'models', 'vosk')
 PDF_STORY_PATH = os.path.join(MAIN_FOLDER_PATH, 'Alice_Story.pdf')
 
+# Call the ReadWithMe API
 readWithMe = ReadWithMe("English(US)", "vosk", VOSK_PATH) 
-#TODO: create class with true/false for each functionality, based on json file values
 
 # Word Sync
-readWithMe.karaoke_reading_by_words()
+#readWithMe.karaoke_reading_by_words()
 
 # Phrase Sync
 #readWithMe.karaoke_reading_by_phrases()
@@ -21,3 +22,10 @@ readWithMe.karaoke_reading_by_words()
 # Preprocess the story books and Load the Metaverse for each of them (call this just once per book to store the image, preprocess the story book etc.)
 metaverse = MetaverseGenerator(PDF_STORY_PATH)
 #metaverse.process_text_load_metaverse()
+
+with open('configuration.json') as json_file:
+    data = json.load(json_file)
+
+for mode, value in data['read_mode'].items():
+    if value == True:
+        readWithMe.karaoke_sync_reading(mode)
--- readWithMe.py
+++ readWithMe.py
@@ -7,12 +7,14 @@ class ReadWithMe: #kita
         self.finalModel = VoskModel(self.lang, model_path)
 
   
-    def karaoke_reading_by_words(self):
-        self.finalModel.karaoke_reading("word_sync")
+    #def karaoke_reading_by_words(self):
+    #    self.finalModel.karaoke_reading("word_sync")
         
-    def karaoke_reading_by_phrases(self):
-        self.finalModel.karaoke_reading("phrase_sync")
+    #def karaoke_reading_by_phrases(self):
+    #    self.finalModel.karaoke_reading("phrase_sync")
 
-    def karaoke_reading_by_context(self):
-        self.finalModel.karaoke_reading("context_sync")
+    #def karaoke_reading_by_context(self):
+    #    self.finalModel.karaoke_reading("context_sync")
 
+    def karaoke_sync_reading(self, mode):
+        self.finalModel.karaoke_reading(mode)
