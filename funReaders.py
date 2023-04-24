e46875fbe5a5fc1d177b166609eabb5a04e969d4
Alexandra Irimia <zcabair@ucl.ac.uk>
2023-04-25 00:12:17 +0100

    Clean and document the entry API of the Reading App v2

--- funReaders.py
+++ funReaders.py
@@ -1,4 +1,3 @@
-import sys 
 from kita import Kita
 from storyBookPreprocessor import StoryBookPreprocessor
 import os
@@ -7,20 +6,16 @@ MAIN_FOLDER_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath
 VOSK_PATH = os.path.join(MAIN_FOLDER_PATH, 'models', 'vosk')
 STORIES_LIBRARY = os.path.join(MAIN_FOLDER_PATH,  "Story Library")
 
-#lang = sys.argv[1]
-#model = sys.argv[2]
-#mode = sys.argv[3]
-#model_path = sys.argv[4]
-
-# For the karaoke reading powered by the speech engine
+# The ReadAlong API (with Karaoke Reading Tracker and Pronunciation  Checker) is powered by KITA
 speech_engine = Kita("English(US)", "vosk", VOSK_PATH) 
 print("The call to the Kita API was successful!")
 speech_engine.reading_with_kita()
 
-# For preprocessing the books - retrieve and store the images
+# Metaverse Back-end (with Book Preprocessor, Image Generator and Metaverse Generator) is powered by OpenAI's GPT 3 - DALLE 2
 for story in os.listdir(STORIES_LIBRARY):
    if os.path.isfile(os.path.join(STORIES_LIBRARY, story)):
-       if os.path.exists(os.path.join(MAIN_FOLDER_PATH, 'JSON Images Filestore', story)) == False: # the book doesn't have the images generated yet
+       # Render the images for a book which doesn't have them generated yet
+       if os.path.exists(os.path.join(MAIN_FOLDER_PATH, 'JSON Images Filestore', story)) == False: 
         PDF_STORY_PATH = os.path.join(STORIES_LIBRARY, story)
         metaverse = StoryBookPreprocessor(PDF_STORY_PATH, story) 
         metaverse.process_text_load_metaverse() 
